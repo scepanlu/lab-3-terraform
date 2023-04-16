955dd6fb04303fba6d8be265456508708b406327
scepanlu <122572423+scepanlu@users.noreply.github.com>
2023-04-16 17:11:07 -0400

    Update compute_main.tf

--- compute/compute_main.tf
+++ compute/compute_main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "EC2_public" {
   instance_type     = "t2.micro"
   subnet_id         = var.networking_var_public_subnet
   availability_zone = "us-east-1a"
-  user_data         = file("/home/ec2-user/environment/lab_3/compute/apache.sh")
+  user_data         = file("${path.module}/apache.sh")
   key_name          = "LukaVMKey"
   tags              = { Name = "EC2_public" }
 }
@@ -13,7 +13,7 @@ resource "aws_instance" "EC2_private" {
   instance_type     = "t2.micro"
   subnet_id         = var.networking_var_private_subnet
   availability_zone = "us-east-1b"
-  user_data         = file("/home/ec2-user/environment/lab_3/compute/apache.sh")
+  user_data         = file("${path.module}/apache.sh")
   key_name          = "LukaVMKey"
   tags              = { Name = "EC2_public" }
 }
